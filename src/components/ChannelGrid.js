8e419117f4f1b5d573fd72b4265c4889edb06e79
Ros McMahon <rosmcmahon@gmail.com>
2020-08-02 00:40:39 +0100

    make grid horizonal scroll when no space

--- src/components/ChannelGrid.js
+++ src/components/ChannelGrid.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import Channel from'./Channel';
+import './ChannelGrid.css'
 
 export default function ChannelGrid ({chans, stepChange, onChangeGain}) {
 	let i = 0; //we're using the map like a for-loop
 	return (
-		<div style={styleGrid}>
-			{
-				chans.map(() => (
-					<React.Fragment key={""+i}> 
-						<Channel 
-							gain={chans[i].gain}
-							name={chans[i].name}
-							channel={i} 
-							seq={chans[i++].sequence} 
-							stepChange={stepChange}
-							onChangeGain={onChangeGain}
-						/>
-						<br />
-					</React.Fragment>
-				))
-			}
+		<div className='grid-container'>
+			<div className='grid-scroller'>
+				{
+					chans.map(() => (
+						<React.Fragment key={""+i}> 
+							<Channel 
+								gain={chans[i].gain}
+								name={chans[i].name}
+								channel={i} 
+								seq={chans[i++].sequence} 
+								stepChange={stepChange}
+								onChangeGain={onChangeGain}
+							/>
+							<br />
+						</React.Fragment>
+					))
+				}
+			</div>
 		</div>
 	)
 
 
 }
 
-const styleGrid = { 
-	minWidth: 590, 
-	overflow: 'scroll', 
-	textAlign: 'initial' 
-}
